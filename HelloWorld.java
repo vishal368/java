994b396498689516a35dd47065f6ba24a9c9683f
vishal <66064021+vishal368@users.noreply.github.com>
2025-11-21 12:07:50 +0530

    This is 1st build to automate

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hi vishal welocme to the DevOps Era");
+      System.out.println("Hi vishal This is 1st build to automate");
    }
 }
