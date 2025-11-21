a302f194345b73cd8a865ac2c3d6308d6890216b
vishal <vishalshamkaria@gmail.com>
2025-11-21 12:24:00 +0530

    git to github

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hi vishal This is 1st build to automate");
+      System.out.println("git to github to jenkins");
    }
 }
